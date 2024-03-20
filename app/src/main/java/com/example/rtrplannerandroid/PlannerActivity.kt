bc67e8bf94e25570fb5dd412c363cd22f703ba69
Jason.Smits <jasonasmits@gmail.com>
2024-03-20 10:14:11 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/rtrplannerandroid/PlannerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rtrplannerandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class PlannerActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
